4a7908ae2b48dfdbfff39b27154b45a1a117ab6e
sid-patri-fable <155963795+sid-patri-fable@users.noreply.github.com>
2024-02-23 23:21:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,9 @@ module.exports = {
     "Capturing-Demos/Using-the-Extension.mdx",
     "Capturing-Demos/Adding-Additional-Captures.mdx",
     "Editing-Demos",
-    "Editing-Demos/Overview.mdx"
+    "Editing-Demos/Overview.mdx",
+    "Editing-Demos/Types-of-Annotations.mdx",
+    "Editing-Demos/Editing-a-Text-Annotation-Message.mdx"
   ],
   layout: CustomLayout,
   props: {
