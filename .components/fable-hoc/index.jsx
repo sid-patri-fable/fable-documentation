9bcadd866c86c7a9bc9d7aef78c68dfcefd47864
Shubham Sharma <shubham-int@sharefable.com>
2024-03-06 12:21:19 +0530

    add: fable hoc comp

---
+++ .components/fable-hoc/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Fable from 'fable-hoc'
+
+export const FableHoc = () => {
+  return (
+    <Fable
+      layout="sidebyside"
+      demoRid="test-get2pic0iiosnvbo"
+      origin="http://localhost:3000"
+      contentWidthPercentage={51}
+    />
+  )
+}
