91d29c0593765c7f5e99f9869e1ccf023bd1c43a
sid-patri-fable <155963795+sid-patri-fable@users.noreply.github.com>
2024-05-20 13:09:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -28,6 +28,7 @@ module.exports = {
     "Editing-Demos/Types-of-Guides.mdx",
     "Editing-Demos/Editing-a-Text-Guide-Message.mdx",
     "Editing-Demos/Video-Guide-Message.mdx",
+    "Editing-Demos/Multi-Guide.mdx",
     "Editing-Demos/Sizing-and-Positioning.mdx",
     "Editing-Demos/Branding.mdx",
     "Editing-Demos/Call-to-Actions.mdx",
