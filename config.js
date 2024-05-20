0b48e91b7ba84c4ccc709ae787fb13ccbf242ca0
sid-patri-fable <155963795+sid-patri-fable@users.noreply.github.com>
2024-05-20 11:59:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -20,6 +20,7 @@ module.exports = {
     "Miscellaneous",
     "Introduction/Welcome-to-Fable.mdx",
     "Introduction/Quick-Start-Guide.mdx",
+    "Introduction/Installing-the-Chrome-Extension.mdx",
     "Capturing-Demos/Overview.mdx",
     "Capturing-Demos/Using-the-Extension.mdx",
     "Capturing-Demos/Adding-Additional-Captures.mdx",
