2d31f23ab2982e1b0919672b21c33f97c8d35b1a
Kedar Basutkar <kedar@sharefable.com>
2024-08-12 15:36:38 +0530

    force deploy

--- analytics.js
+++ analytics.js
@@ -3,7 +3,7 @@
 const addGTMandGA = () => {
   const head = document.querySelector("head");
   const body = document.querySelector("body");
-
+ 
   const gtm = `<script>(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
