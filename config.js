d4dc81514940717d287266cf910ab6c04cbd855b
Kedar Basutkar <kedar@sharefable.com>
2024-08-21 16:37:59 +0530

    add hubspot integration page

--- config.js
+++ config.js
@@ -65,6 +65,7 @@ module.exports = {
     "Settings-and-Preferences/How-to-host-the-demos-on-my-domain.mdx",
     "Manage-billing/Manage-subscriptions.mdx",
     "Integrate-Demos-with-your-stack/Overview.mdx",
+    "Integrate-Demos-with-your-stack/How-to-integrate-with-hubspot.mdx",
   ],
   layout: CustomLayout,
   props: {
