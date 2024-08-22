2d81cd1480a03509c197d71933bead36ec9288e0
Kedar Basutkar <kedar@sharefable.com>
2024-08-22 15:38:21 +0530

    add troubleshooting page

--- config.js
+++ config.js
@@ -63,6 +63,7 @@ module.exports = {
     "Settings-and-Preferences/Overview.mdx",
     "Settings-and-Preferences/Add-global-styling-to-all-your-demos.mdx",
     "Settings-and-Preferences/How-to-host-the-demos-on-my-domain.mdx",
+    "Settings-and-Preferences/Troubleshoot-domain-mapping-issues.mdx",
     "Manage-billing/Manage-subscriptions.mdx",
     "Integrate-Demos-with-your-stack/Overview.mdx",
     "Integrate-Demos-with-your-stack/How-to-integrate-with-hubspot.mdx",
