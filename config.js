0abd1cd30041e956d405b80d1541b79f769aa5fc
Kedar Basutkar <kedar@sharefable.com>
2024-08-27 16:04:03 +0530

    add single select page

--- config.js
+++ config.js
@@ -101,6 +101,7 @@ module.exports = {
     "Demo-Hub/Body-configurations.mdx",
     "Demo-Hub/Manage-sections.mdx",
     "Demo-Hub/Qualifications.mdx",
+    "Demo-Hub/Single-select.mdx",
     "Demo-Hub/Publish-demo-hub.mdx",
   ],
   layout: CustomLayout,
