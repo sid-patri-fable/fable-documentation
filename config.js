7f28dc57102ea68b3b1c73e408341e8fb8750b92
Kedar Basutkar <kedar@sharefable.com>
2024-09-17 12:51:44 +0530

    more changes

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ import Header from "./.components/header/Header.jsx";
 import CustomLayout from "./.components/custom-layout/CustomLayout.jsx";
 import CustomSidepanel from "./.components/sidepanel/index.jsx";
 import Footer from "./.components/footer/Footer.jsx";
- 
+
 module.exports = {
   version: "1.0.0",
   urlMapping: {
@@ -234,6 +234,23 @@ module.exports = {
       copyright: "Fable © 2024",
       links: [
         {
+          heading: "Product",
+          links: [
+            {
+              title: "Interactive Demo",
+              url: "https://www.sharefable.com/products/interactive-demo",
+            },
+            {
+              title: "Demo Hub",
+              url: "https://www.sharefable.com/products/demo-hub",
+            },
+            {
+              title: "Quilly",
+              url: "https://www.sharefable.com/products/quilly",
+            },
+          ],
+        },
+        {
           heading: "Solutions",
           links: [
             {
@@ -329,24 +346,24 @@ module.exports = {
       ],
       g2Badges: [
         {
-         source: 'https://www.sharefable.com/g2-badges/HighPerformer.svg',
-         name: 'High Performer',
+          source: 'https://www.sharefable.com/g2-badges/HighPerformer.svg',
+          name: 'High Performer',
         },
         {
-         source: 'https://www.sharefable.com/g2-badges/EasiestToUse.svg',
-         name: 'Easiest To Use',
+          source: 'https://www.sharefable.com/g2-badges/EasiestToUse.svg',
+          name: 'Easiest To Use',
         },
         {
-         source: 'https://www.sharefable.com/g2-badges/FastestImplementation.svg',
-         name: 'Fastest Implementation',
+          source: 'https://www.sharefable.com/g2-badges/FastestImplementation.svg',
+          name: 'Fastest Implementation',
         },
         {
-         source: 'https://www.sharefable.com/g2-badges/BestEstimatedROI.svg',
-         name: 'Best Estimated ROI',
+          source: 'https://www.sharefable.com/g2-badges/BestEstimatedROI.svg',
+          name: 'Best Estimated ROI',
         },
         {
-         source: 'https://www.sharefable.com/g2-badges/EasiestToDoBusinessWith.svg',
-         name: 'Easiest To Do Business With',
+          source: 'https://www.sharefable.com/g2-badges/EasiestToDoBusinessWith.svg',
+          name: 'Easiest To Do Business With',
         },
       ],
     },
